d8a1e139d5f70da546f2af727a845b2de8430868
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-07-01 16:47:13 +1000

    Add more prices

--- src/oracle.sol
+++ src/oracle.sol
@@ -1,23 +1,26 @@
 pragma solidity ^0.4.9;
 
-contract oracleTest {
-    bytes32 public rate;
+// contract oracleTest {
+//     bytes32 public rate;
 
-    function otest() {
-        //use brave new coin oracle 0x25dc90faa727aa29e437e660e8f868c9784d3828, checked sum 0x25Dc90FAa727aa29e437E660e8F868C9784D3828
-        Oracle o = Oracle(0x25Dc90FAa727aa29e437E660e8F868C9784D3828);
-        rate = o.current();
-    }
-}
+//     function otest() {
+//         //use brave new coin oracle 0x25dc90faa727aa29e437e660e8f868c9784d3828, checked sum 0x25Dc90FAa727aa29e437E660e8F868C9784D3828
+//         Oracle o = Oracle(0x25Dc90FAa727aa29e437E660e8F868C9784D3828);
+//         rate = o.current();
+//     }
+// }
 
 contract Oracle{
-    bytes32 public _current;
+    uint256 public _DayAvgPrice;
+    uint256 public _DayHighestPrice;
+    uint256 public _DayLowestPrice;
+    uint256 public _LastPrice;
     
 	function Oracle()
     {
     }
 
-	function update(bytes32 newCurrent)
+	function update(uint256 dayAvgPrice, uint256 dayHighestPrice, uint256 dayLowestPrice, uint256 lastPrice)
     {
         _current = newCurrent;
     }
