6a57468e0938d0aee6ed97097be8a9453ccd6605
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-07-01 18:53:29 +1000

    Added all values with msg.sender

--- src/oracle.sol
+++ src/oracle.sol
@@ -1,32 +1,46 @@
 pragma solidity ^0.4.9;
 
-// contract oracleTest {
-//     bytes32 public rate;
-
-//     function otest() {
-//         //use brave new coin oracle 0x25dc90faa727aa29e437e660e8f868c9784d3828, checked sum 0x25Dc90FAa727aa29e437E660e8F868C9784D3828
-//         Oracle o = Oracle(0x25Dc90FAa727aa29e437E660e8F868C9784D3828);
-//         rate = o.current();
-//     }
-// }
-
 contract Oracle{
     uint256 public _DayAvgPrice;
     uint256 public _DayHighestPrice;
     uint256 public _DayLowestPrice;
     uint256 public _LastPrice;
+
+    address _owner;
     
 	function Oracle()
     {
+        _owner = msg.sender;
     }
 
 	function update(uint256 dayAvgPrice, uint256 dayHighestPrice, uint256 dayLowestPrice, uint256 lastPrice)
     {
-        _current = newCurrent;
+        if (msg.sender == _owner)
+        {
+            _DayAvgPrice = dayAvgPrice;
+            _DayHighestPrice = dayHighestPrice;
+            _DayLowestPrice = dayLowestPrice;
+            _LastPrice = lastPrice;
+        }
     }
 
-	function current() constant returns(bytes32 current)
+	function DayAvgPrice() constant returns(uint256 dayAvgPrice)
     {
-        return _current;
+        return _DayAvgPrice;
+    }
+
+    function DayHighestPrice() constant returns(uint256 dayHighestPrice)
+    {
+        return _DayHighestPrice;
+    }
+
+    function DayLowestPrice() constant returns(uint256 dayLowePrice)
+    {
+        return _DayLowestPrice;
+    }
+
+    function LastPrice() constant returns(uint256 lastPrice)
+    {
+        return _LastPrice;
     }
 }
